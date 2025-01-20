348ab924ea9ae04569a4a077ec956aae64531547
Eloy Francisco Martinez Cabello <eloymc95@gmail.com>
2025-01-20 09:41:28 -0300

    01202025-1

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,71 @@
+// Fraction.cs
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int _top;
+    private int _bottom;
+
+    // Constructor that initializes the fraction to 1/1
+    public Fraction()
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor that initializes the fraction with a whole number (e.g., 5 -> 5/1)
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // Constructor that initializes the fraction with a numerator and denominator
+    public Fraction(int top, int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getter and Setter for the numerator
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    // Getter and Setter for the denominator
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+
+    // Method to get the fraction as a string
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    // Method to get the decimal value of the fraction
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
     }
 }
